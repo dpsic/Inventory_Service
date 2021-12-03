e2ada0467213a6468ce7d18c579a75569e1e9877
softApp-eng <softmed2018@gmail.com>
2021-12-03 13:11:11 +0100

    Initial commit

---
+++ src/main/java/com/example/inventory_service/InventoryServiceApplication.java
@@ -0,0 +1,27 @@
+package com.example.inventory_service;
+
+import com.example.inventory_service.entities.Product;
+import com.example.inventory_service.respository.ProductRepo;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryServiceApplication.class, args);
+    }
+    @Bean
+    CommandLineRunner start(ProductRepo productRepo)
+    {
+        return args ->{
+            productRepo.save(new Product(null,"ordinateur",788,12));
+            productRepo.save(new Product(null,"smart phone",148,15));
+            productRepo.save(new Product(null,"imprimante",120,100));
+            productRepo.findAll().forEach(c->{
+                System.out.println(c.getName());            });
+        };
+    }
+}
