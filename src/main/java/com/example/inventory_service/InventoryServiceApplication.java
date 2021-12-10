5d1ce0db0fb24f2bf941135bf46f7abf721b1e81
softApp-eng <softmed2018@gmail.com>
2021-12-10 12:52:11 +0100

    MODIFICATION

--- src/main/java/com/example/inventory_service/InventoryServiceApplication.java
+++ src/main/java/com/example/inventory_service/InventoryServiceApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.repository.config.RepositoryConfiguration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
@@ -14,8 +16,8 @@ public class InventoryServiceApplication {
         SpringApplication.run(InventoryServiceApplication.class, args);
     }
     @Bean
-    CommandLineRunner start(ProductRepo productRepo)
-    {
+    CommandLineRunner start(ProductRepo productRepo , RepositoryRestConfiguration restConfiguration)
+    {restConfiguration.exposeIdsFor(Product.class);
         return args ->{
             productRepo.save(new Product(null,"ordinateur",788,12));
             productRepo.save(new Product(null,"smart phone",148,15));
